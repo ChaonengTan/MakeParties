5a46e1c79680796781949f25903758778f605433
Chaoneng Tan <chaonengtan@gmail.com>
2022-06-17 23:12:53 -0700

    Users can delete rsvps

--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -10,5 +10,13 @@ module.exports = (app, models) => {
   
     // CREATE
   
-    // DESTROY  
+    // DELETE
+    app.delete('/events/:eventId/rsvps/:id', (req, res) => {
+        models.Rsvp.findByPk(req.params.id).then(rsvp => {
+            rsvp.destroy();
+            res.redirect(`/events/${req.params.eventId}`);
+        }).catch((err) => {
+            console.log(err);
+        });
+    });
   }
